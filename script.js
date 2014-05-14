02123b076af6be96738f3896372be3dfee43a0ba
Suyeon Son <suyeon.son@icloud.com>
2014-05-14 01:52:29 -0500

    fix heights

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ $(function() {
 	$("#button").delay(2000).fadeIn("slow");
 });
 
-$('.thing').waypoint(function(direction) {
-  alert('Top of thing hit top of viewport.');
+$(function () {
+	$("button").on("click", function() {
+		$.scrollTo(".container", 800);
+	});
 });
+// $('.container').waypoint(function(direction) {
+//   alert('Top of thing hit top of viewport.');
+// });
