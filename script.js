ac0d6907a6d3ab2d6f6486e06546891c0a221caf
Suyeon Son <suyeon.son@icloud.com>
2014-05-20 21:48:45 -0500

    it's vaguely what we want in terms of stickiness

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-$(function() {
-	$("#bubble2").delay(1000).fadeIn("slow");
-});
-
-$(function() {
-	$("#byline").delay(2000).fadeIn("slow");
-});
-
-$(function() {
-	$("#teaser").delay(2000).fadeIn("slow");
-});
-
-$(function() {
-	$("#button").delay(2000).fadeIn("slow");
-});
-
-$(function () {
-	$("button").on("click", function() {
-		$.scrollTo(".container", 800);
+$(document).ready(function () {
+	$(function() {
+		$("#bubble2").delay(1000).fadeIn("slow");
 	});
+
+	$(function() {
+		$("#byline").delay(2000).fadeIn("slow");
+	});
+
+	$(function() {
+		$("#teaser").delay(2000).fadeIn("slow");
+	});
+
+	$(function() {
+		$("#button").delay(2000).fadeIn("slow");
+	});
+
+	$(function () {
+		$("button").on("click", function() {
+			$.scrollTo(".container", 800);
+		});
+	});	
+
+	$("#sec1").waypoint("sticky", {
+		offset: 50
+	});
+
+	$("#subhead2").waypoint(function() {
+		$("#sec1").waypoint("unsticky")
+	});
+
+	$("#sec2").waypoint("sticky", {
+		offset: 50
+	});
+
+	$("#subhead3").waypoint(function() {
+		$("#sec2").waypoint("unsticky")
+	});
+
+	$("#sec3").waypoint("sticky", {
+		offset: 50
+	});
+
 });
+
+
+
 
 // use this for nav bar changes
 // $(function() {
